b3a4a32cff922696bba90eee5daee228d6be9cda
AkhilaMarapalli17 <marapalliakhila17@gmail.com>
2025-12-13 11:54:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Search Pattern/search-pattern.java
+++ Difficulty: Hard/Search Pattern/search-pattern.java
@@ -1,50 +1,55 @@
+import java.util.ArrayList;
+
 class Solution {
-    private int[] buildLPS(String pattern){
-        int m=pattern.length();
-        int[] lps=new int[m];
-        int j=1,len=0;
-        while(j<m){
-            if(pattern.charAt(len)==pattern.charAt(j)){
-                len++;
-                lps[j]=len;
-                j++;
-            }
-            else{   
-                if(len!=0)len=lps[len-1];
-                else{
-                    lps[j]=0;
-                    j++;
-                }
-            }
-        }
-        return lps;
-    }
-    private ArrayList<Integer> kmp(String text, String pattern) {
-        int n = text.length(), m = pattern.length();
-        ArrayList<Integer> ans = new ArrayList<>();
-        if (m > n) return ans;
-        int lps[]=buildLPS(pattern);
-        int i=0,j=0;
-        while(i<n){
-            if(text.charAt(i)==pattern.charAt(j)){
+    ArrayList<Integer> search(String pat, String txt) {
+        int m = pat.length();
+        int n = txt.length();
+
+        int[] lps = new int[m];
+        ArrayList<Integer> res = new ArrayList<>();
+
+        constructLps(pat, lps);
+
+        int i = 0; // index for txt
+        int j = 0; // index for pat
+
+        while (i < n) {
+            if (txt.charAt(i) == pat.charAt(j)) {
                 i++;
                 j++;
-                if(j==m){
-                    ans.add(i-m);
-                    j=lps[j-1];
+                if (j == m) {
+                    res.add(i - j); // match found
+                    j = lps[j - 1];
+                }
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
                 }
             }
-            else{
-                if(j!=0)j=lps[j-1];
-                else i++;
+        }
+        return res;
+    }
+
+    public void constructLps(String pat, int[] lps) {
+        int len = 0;
+        lps[0] = 0;
+        int i = 1;
+
+        while (i < pat.length()) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
-        return ans;
-    }
-
-    ArrayList<Integer> search(String pat, String txt) {
-        return kmp(txt, pat);
     }
 }
-
- 
