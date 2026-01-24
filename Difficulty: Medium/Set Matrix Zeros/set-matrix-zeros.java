b7bdb2b634722c7d0c617d2435d6fd2cde84bf77
AkhilaMarapalli17 <marapalliakhila17@gmail.com>
2026-01-24 11:12:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Set Matrix Zeros/set-matrix-zeros.java
+++ Difficulty: Medium/Set Matrix Zeros/set-matrix-zeros.java
@@ -1,26 +1,44 @@
 class Solution {
     public void setMatrixZeroes(int[][] mat) {
-        // code here
-        int n=mat.length;
-        int m=mat[0].length;
-        boolean[] rows = new boolean[n];
-        boolean[] cols = new boolean[m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j] == 0){
-                    rows[i] = true;
-                    cols[j] = true;
+
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int c0 = 1;
+
+   
+        for (int i = 0; i < n; i++) {
+            if (mat[i][0] == 0) c0 = 0;
+
+            for (int j = 1; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    mat[i][0] = 0;
+                    mat[0][j] = 0;
                 }
             }
         }
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(rows[i] || cols[j]){
-                    mat[i][j] =0;
+
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (mat[i][0] == 0 || mat[0][j] == 0) {
+                    mat[i][j] = 0;
                 }
             }
         }
-        
+
+
+        if (mat[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                mat[0][j] = 0;
+            }
+        }
+
+
+        if (c0 == 0) {
+            for (int i = 0; i < n; i++) {
+                mat[i][0] = 0;
+            }
+        }
     }
 }
